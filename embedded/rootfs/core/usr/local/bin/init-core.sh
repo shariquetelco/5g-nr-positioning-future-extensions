238fa2c2e4f1ea163f3f6cecf24a2e1abf5f82ce
Nico Kalis <nico.kalis@uni-rostock.de>
2025-04-22 08:40:20 +0000

    [Linux] Added root filesystem and boot image resources

---
+++ embedded/rootfs/core/usr/local/bin/init-core.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# (1) Mount pseudo filesystems
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -o rw,remount /
+
+# (2) Initialize network interfaces
+ifconfig eth0 192.168.3.1
+ifconfig eth0 netmask 255.255.255.0
+ifconfig lo 127.0.0.1
+
+# (3) Load required kernel modules
+modprobe sctp
+
+# (4) Set date and time of the system (required for valid TLS certificates!)
+date -s "2024-08-09 10:00:00"
+
+# (5) Network tuning within the Linux kernel
+echo 4096 > /proc/sys/net/core/somaxconn
+echo 65536 > /proc/sys/net/core/netdev_max_backlog
+echo 0 > /proc/sys/net/ipv4/tcp_slow_start_after_idle
+echo 65536 > /proc/sys/net/ipv4/tcp_max_syn_backlog
+echo 0 > /proc/sys/net/ipv4/tcp_syncookies
+echo 16777216 > /proc/sys/net/core/rmem_max
+echo 16777216 > /proc/sys/net/core/wmem_max
+echo 16777216 > /proc/sys/net/core/rmem_default
+echo 16777216 > /proc/sys/net/core/wmem_default
+echo 40960 > /proc/sys/net/core/optmem_max
+
+# (6) CPU frequency scaling
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo performance > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo performance > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
+# (7) Disable RT throttling feature
+#echo -1 > /proc/sys/kernel/sched_rt_runtime_us
